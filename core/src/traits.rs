7e2bb887ab659522ae5c474a51c0a6f718b62ef6
vibhurajeev <vibhurajeev@gmail.com>
2024-03-07 16:38:16 +0400

    feat(core): agg types

--- core/src/traits.rs
+++ core/src/traits.rs
@@ -1,3 +1,13 @@
+use crate::agg_types::{AggregatedTransaction, SubmitProofTransaction};
+use anyhow::{anyhow, Error};
+
 pub trait Leaf<K> {
     fn get_key(&self) -> K;
+}
+
+pub trait Aggregator {
+    fn aggregate(
+        &self,
+        submit_proof_txs: Vec<SubmitProofTransaction>,
+    ) -> Result<AggregatedTransaction, Error>;
 }
