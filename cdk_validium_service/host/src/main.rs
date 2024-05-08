0da55439e66fd7f6df0b3a552838fe01c445d305
nlok5923 <nlokhande5923@gmail.com>
2024-05-08 21:31:32 +0530

    spin off service with demo proof for cdk

---
+++ cdk_validium_service/host/src/main.rs
@@ -0,0 +1,22 @@
+use adapter_sdk::{state::AdapterState, types::AdapterConfig, service::server};
+use cdk_validium_core::DemoProof;
+use cdk_validium_methods::{ADAPTER_ELF, ADAPTER_ID};
+use nexus_core::types::{AppId, StatementDigest};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+fn main() {
+    let mut adapter: AdapterState<DemoProof> = AdapterState::new(
+        String::from("adapter_store"),
+        AdapterConfig {
+            app_id: AppId(100),
+            elf: ADAPTER_ELF.to_vec(),
+            adapter_elf_id: StatementDigest(ADAPTER_ID),
+            vk: [0u8; 32],
+            rollup_start_height: 606460,
+        },
+    );
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
+    rt.block_on(server(Arc::new(Mutex::new(adapter))));
+}
---
+++ cdk_validium_service/methods/guest/src/main.rs
@@ -0,0 +1,33 @@
+#![no_main]
+// If you want to try std support, also update the guest Cargo.toml file
+// std support is experimental
+
+use adapter_sdk::adapter_zkvm::verify_proof;
+use adapter_sdk::types::AdapterPrivateInputs;
+use adapter_sdk::types::AdapterPublicInputs;
+use adapter_sdk::types::RollupProof;
+use cdk_validium_core::DemoProof;
+use nexus_core::types::StatementDigest;
+use risc0_zkvm::guest::env;
+use risc0_zkvm::sha::Digest;
+
+risc0_zkvm::guest::entry!(main);
+
+fn main() {
+    let prev_adapter_public_inputs: Option<AdapterPublicInputs> = env::read();
+    let proof: Option<RollupProof<DemoProof>> = env::read();
+    let private_inputs: AdapterPrivateInputs = env::read();
+    let img_id: StatementDigest = env::read();
+    let vk: [u8; 32] = env::read();
+
+    let result = verify_proof(
+        proof,
+        prev_adapter_public_inputs,
+        private_inputs,
+        img_id,
+        vk,
+    )
+    .unwrap();
+
+    env::commit(&result);
+}
