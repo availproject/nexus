16328f00df74d59d5a832b2cb0d0e449494d30e3
rishotics <rishotics@gmail.com>
2024-07-26 20:55:55 +0530

    add sp1 in nexus prover

--- nexus/prover/guest/src/main.rs
+++ nexus/prover/guest/src/main.rs
@@ -3,6 +3,11 @@ use nexus_core::prover::run;
 use nexus_core::zkvm::risczero::ZKVM;
 risc0_zkvm::guest::entry!(main);
 
+#[cfg(any(feature = "spone"))]
+use nexus_core::zkvm::spone::SZKVM;
+#[cfg(any(feature = "spone"))]
+sp1_zkvm::entrypoint!(main);
+
 fn main() {
     run::<ZKVM>()
 }
