de8489a63d7cb0c63af72752b8d1992cebd77120
Prabhat1308 <prabhatverma329@gmail.com>
2024-09-14 14:58:04 +0530

    add dependencies

--- examples/zksync_adapter/methods/src/zksync_prover.rs
+++ examples/zksync_adapter/methods/src/zksync_prover.rs
@@ -2,6 +2,7 @@ use adapter_sdk::types::AdapterPublicInputs;
 use nexus_core::types::H256;
 use zksync_core::types::{CommitBatchInfo, L1BatchWithMetadata};
 use zksync_core::{MockProof, STF};
+use nexus_core::zkvm::traits::ZKVMEnv;
 
 pub fn run<Z: ZKVMEnv>() {
      
