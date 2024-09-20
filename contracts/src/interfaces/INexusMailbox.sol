114a06769da01ff954794a7373b54d93c3522624
rac-sri <rac.sri25@gmail.com>
2024-09-21 02:35:53 +0530

    feat: mailbox init

---
+++ contracts/src/interfaces/INexusMailbox.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity ^0.8.21;
+
+struct Receipt { 
+     bytes32 chainIdFrom;
+     bytes32[] chainIdTo;
+     bytes data;
+     address from;
+     address[] to; // if specified on verification, the callback on "to" function will be called
+     address[] sm; // security module
+     uint256 nonce;
+}
+
+interface INexusMailbox { 
+    function receiveMessage(uint256 chainblockNumber, Receipt calldata, bytes calldata proof, bool callback) external;
+}
+
---
+++ contracts/src/verification/ethereum/VerifierWrapper.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.21;
+
+import {EthereumVerifier} from "./Verifier.sol";
+import {INexusVerifierWrapper} from "../../interfaces/INexusVerifierWrapper.sol";
+import {INexusProofManager} from "../../interfaces/INexusProofManager.sol";
+
+contract VerifierWrapper is  INexusVerifierWrapper, EthereumVerifier { 
+     bytes32 immutable  chainId;
+     INexusProofManager immutable nexus;
+     struct Proof {
+        bytes accountProof;
+        address addr;
+        bytes storageProof;
+        bytes32 storageSlot;
+    }
+     
+    bytes32 private constant EMPTY_TRIE_ROOT_HASH = 0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421;
+
+    constructor(bytes32 _chainId, INexusProofManager _nexus) EthereumVerifier(_nexus) {
+        chainId = _chainId;
+        nexus = _nexus;
+    }
+
+    function parseAndVerify(uint256 chainblockNumber, bytes32 receipt, bytes calldata data) external { 
+        Proof memory proof;
+        (proof.accountProof, proof.addr, proof.storageProof, proof.storageSlot) = abi.decode(data, (bytes, address, bytes, bytes32));
+        bytes32 state = nexus.getChainState(chainblockNumber, chainId); 
+        (, , , bytes32 storageRoot) = verifyAccount(state, proof.accountProof, proof.addr);
+        require(storageRoot != EMPTY_TRIE_ROOT_HASH, "invalid entry");
+        bytes32 value = verifyStorage(storageRoot, proof.storageSlot, proof.storageProof);
+        require(value == receipt, "Invalid slot value");
+    }
+}
