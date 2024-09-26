b4476b370686ed56a9ca0c7d344fa7cf69ddbb1c
vibhurajeev <vibhurajeev@gmail.com>
2024-09-26 12:20:54 +0400

    feat(nexus-js): add TypeScript SDK for Nexus

---
+++ nexus_js/src/mailbox.ts
@@ -0,0 +1,29 @@
+interface ChainDetails {
+  rpcUrl: string;
+  mailboxContract: string;
+  stateManagerContract: string;
+  appID: string;
+}
+
+export default class MailBoxClient {
+  private chains: Map<string, ChainDetails>;
+
+  constructor(chains: { [key: string]: ChainDetails }) {
+    // Ensure that at least one mapping is provided during initialization
+    if (Object.keys(chains).length === 0) {
+      throw new Error("At least one account state mapping must be provided.");
+    }
+
+    this.chains = new Map();
+    for (let key in chains) {
+      if (chains.hasOwnProperty(key)) {
+        this.chains.set(key, chains[key]);
+      }
+    }
+  }
+
+  addChain(key: string, chainDetails: ChainDetails): void {
+    this.chains.set(key, chainDetails);
+  }
+}
+
