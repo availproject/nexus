2ae1cd27d7122b2ea2f04b6c882deef783f1be37
nlok5923 <nlokhande5923@gmail.com>
2025-01-23 01:08:07 +0530

    fix: conflicts

---
+++ examples/mock_geth_adapter/bench_setup/src/main.rs
@@ -0,0 +1,121 @@
+use adapter_sdk::{api::NexusAPI, types::AdapterConfig};
+use anyhow::{Context, Error};
+use geth_methods::{ADAPTER_ELF, ADAPTER_ID};
+use nexus_core::db::NodeDB;
+use nexus_core::types::{
+    AccountState, AccountWithProof, AppAccountId, AppId, InitAccount, NexusHeader, NexusRollupPI, Proof, StatementDigest, SubmitProof, Transaction, TxParams, TxSignature, H256
+};
+use nexus_core::zkvm::risczero::RiscZeroProof;
+use nexus_core::zkvm::ProverMode;
+use risc0_zkvm::guest::env;
+use risc0_zkvm::serde::to_vec;
+use risc0_zkvm::{default_prover, ExecutorEnv};
+use serde::{Deserialize, Serialize};
+use std::env::args;
+use std::fs;
+use std::time::Duration;
+use std::time::{SystemTime, UNIX_EPOCH};
+use web3::transports::Http;
+use web3::types::BlockId;
+use web3::Web3;
+
+// Your NodeDB struct and methods implementation here
+
+#[derive(Clone, Serialize, Deserialize)]
+struct AdapterStateData {
+    last_height: u32,
+    adapter_config: AdapterConfig,
+}
+
+#[tokio::main]
+async fn main() {
+    let prover_mode = ProverMode::MockProof;
+    let nexus_api = NexusAPI::new(&"http://127.0.0.1:7001");
+
+    for txn_index in 0..100 {
+        let adapter_config = AdapterConfig {
+            app_id: AppId(txn_index),
+            elf: ADAPTER_ELF.to_vec(),
+            adapter_elf_id: StatementDigest(ADAPTER_ID),
+            vk: [0u8; 32],
+            rollup_start_height: 606460,
+            prover_mode: prover_mode.clone(),
+            avail_url: String::from("wss://turing-rpc.avail.so:443/ws"),
+        };
+    
+        // Retrieve or initialize the adapter state data from the database
+        let adapter_state_data = AdapterStateData {
+            last_height: 0,
+            adapter_config,
+        };
+    
+        // Main loop to fetch headers and run adapter
+        let mut last_height = adapter_state_data.last_height;
+        let mut start_nexus_hash = None;
+
+        let app_account_id = AppAccountId::from(adapter_state_data.adapter_config.app_id.clone());
+        let account_with_proof: AccountWithProof =
+            match nexus_api.get_account_state(&app_account_id.as_h256()).await {
+                Ok(i) => i,
+                Err(e) => {
+                    println!("{:?}", e);
+                    continue;
+                }
+            };
+
+        last_height = account_with_proof.account.height;
+        let file_name = format!("headers/nexus_header.json");
+        let file_content = fs::read_to_string(file_name).unwrap();
+        let header: NexusHeader = serde_json::from_str(&file_content).unwrap();
+
+        let height: u32 = 30; // random height
+        let public_inputs = NexusRollupPI {
+            nexus_hash: header.hash(),
+            state_root: H256::zero(),
+            height,
+            start_nexus_hash: start_nexus_hash
+                .unwrap_or_else(|| H256::from(account_with_proof.account.start_nexus_hash)),
+            app_id: app_account_id.clone(),
+            img_id: StatementDigest(ADAPTER_ID),
+            rollup_hash: Some(H256::zero()),
+        };
+
+        let mut env_builder = ExecutorEnv::builder();
+        let env = env_builder.write(&public_inputs).unwrap().build().unwrap();
+        let prover = default_prover();
+        let prove_info = match prover.prove(env, ADAPTER_ELF) {
+            Ok(i) => i,
+            Err(e) => {
+                println!("Unable to generate proof due to error: {:?}", e);
+                continue;
+            }
+        };
+
+        let recursive_proof = RiscZeroProof(prove_info.receipt);
+
+        let tx = Transaction {
+            signature: TxSignature([0u8; 64]),
+            params: TxParams::SubmitProof(SubmitProof {
+                app_id: app_account_id.clone(),
+                nexus_hash: header.hash(),
+                state_root: public_inputs.state_root.clone(),
+                proof: match recursive_proof.clone().try_into() {
+                    Ok(i) => i,
+                    Err(e) => {
+                        println!("Unable to serialise proof: {:?}", e);
+                        continue;
+                    }
+                },
+                height: public_inputs.height,
+                data: None,
+            }),
+        };
+
+        let json_string = serde_json::to_string_pretty(&tx).unwrap();
+        let file_name = format!(
+            "src/submit_proof_transactions/submit_proof_transaction_{}.json",
+            txn_index
+        );
+        fs::write(file_name, json_string).unwrap();
+    };
+}
--- nexus/bench/src/nexus_bench.rs
+++ nexus/bench/src/nexus_bench.rs
@@ -1,19 +1,17 @@
-use adapter_sdk::{api::NexusAPI, types::AdapterConfig};
-use geth_methods::{ADAPTER_ELF, ADAPTER_ID};
+use geth_methods::ADAPTER_ID;
 use nexus_core::{
     db::NodeDB,
     state::vm_state::VmState,
     state_machine::StateMachine,
     types::{
-        AppAccountId, AppId, AvailHeader, DataLookup, Digest, DigestItem, Extension, HeaderStore,
-        InitAccount, KateCommitment, NexusHeader, NexusRollupPI, StatementDigest, SubmitProof,
-        Transaction, TxParams, TxSignature, V3Extension, H256,
+        AppAccountId, AppId, AvailHeader, HeaderStore,
+        InitAccount, NexusHeader, StatementDigest,
+        Transaction, TxParams, TxSignature,
     },
     zkvm::ProverMode,
 };
 use nexus_host::execute_batch;
 use rocksdb::Options;
-use serde::{Deserialize, Serialize};
 use serde_json::from_reader;
 use std::env;
 use std::fs;
@@ -35,15 +33,7 @@ use env_logger;
 #[cfg(any(feature = "sp1"))]
 use log;
 
-#[derive(Clone, Serialize, Deserialize)]
-struct AdapterStateData {
-    last_height: u32,
-    adapter_config: AdapterConfig,
-}
-
-//@TODO : use mockproofs for bench
 fn create_mock_data() -> (
-    Vec<Transaction>,
     StateMachine<ZKVM, Proof>,
     Vec<AvailHeader>,
     HeaderStore,
@@ -71,39 +61,6 @@ fn create_mock_data() -> (
     let mut txs: Vec<Transaction> = Vec::new();
     let state_machine = StateMachine::<ZKVM, Proof>::new(state.clone());
 
-    // Read all file names from the transactions directory
-    let dir_path = "src/transactions";
-    let mut files = fs::read_dir(dir_path)
-        .unwrap()
-        .filter_map(|entry| {
-            entry.ok().and_then(|e| {
-                let path = e.path();
-                if path.is_file() && path.extension().unwrap_or_default() == "json" {
-                    path.file_name()
-                        .map(|name| name.to_string_lossy().into_owned())
-                } else {
-                    None
-                }
-            })
-        })
-        .collect::<Vec<_>>();
-
-    // Sort files to ensure consistent ordering
-    files.sort();
-
-    let num_txns = 300; // desired number of transactions
-    let files_len = files.len(); // actual number of files available
-
-    for tx in 0..num_txns {
-        // Use modulo to loop back to start when we run out of files
-        let file_index = tx % files_len;
-        let file_path = format!("{}/{}", dir_path, files[file_index]);
-        let tx_file = File::open(&file_path).unwrap();
-        let tx_reader = BufReader::new(tx_file);
-        let tx: Transaction = from_reader(tx_reader).unwrap();
-        txs.push(tx);
-    }
-
     let avail_header = File::open("src/avail_header.json").unwrap();
     let avail_header_reader = BufReader::new(avail_header);
     let avail_headers: Vec<AvailHeader> = from_reader(avail_header_reader).unwrap();
@@ -112,33 +69,11 @@ fn create_mock_data() -> (
     let header_store_reader = BufReader::new(header_store);
     let header_store: HeaderStore = from_reader(header_store_reader).unwrap();
 
-    (txs, state_machine, avail_headers, header_store)
+    (state_machine, avail_headers, header_store)
 }
 
-async fn create_mock_transactions() {
-    let (_, mut state_machine, avail_headers, mut header_store) = create_mock_data();
-        let mock_txs: Vec<Transaction> = Vec::new();
-        let prover_mode = ProverMode::NoAggregation;
-
-        let (_, header, _, _) = execute_batch::<Prover, Proof, ZKVM>(
-            &mock_txs,
-            &mut state_machine,
-            &avail_headers[0],
-            &mut header_store,
-            prover_mode.clone(),
-        )
-        .await
-        .unwrap();
-
-        let json_string = serde_json::to_string_pretty(&header).unwrap();
-        fs::write("src/headers/nexus_header_1.json", json_string).unwrap();
-
-        let nexus_header_1 = fs::read_to_string("src/headers/nexus_header_1.json").unwrap();
-
-        // making 100 init transactions with diff app account id and store the results
-        let header_1: NexusHeader = serde_json::from_str(&nexus_header_1).unwrap();
-        println!("Nexus header 1 {:?}", header_1);
-        let nexus_api = NexusAPI::new(&"http://127.0.0.1:7001");
+async fn bench_init_account_transactions(header: NexusHeader, prover_mode: ProverMode, state_machine: &mut StateMachine<ZKVM, Proof>, avail_headers: Vec<AvailHeader>, mut header_store: HeaderStore) -> Proof {
+    let mut init_account_transactions: Vec<Transaction> = Vec::new();
 
         for txn_index in 0..100 {
             let tx = Transaction {
@@ -173,87 +108,17 @@ async fn create_mock_transactions() {
         .await
         .unwrap();
 
-        let json_string = serde_json::to_string_pretty(&header).unwrap();
-        fs::write("src/headers/nexus_header_2.json", json_string).unwrap();
+    // store header into json file
+    let json_string = serde_json::to_string_pretty(&header).unwrap();
+    fs::write("src/headers/nexus_header.json", json_string).unwrap();
 
-        let nexus_header_2 = fs::read_to_string("src/headers/nexus_header_2.json").unwrap();
-        let header_2: NexusHeader = serde_json::from_str(&nexus_header_2).unwrap();
-        println!("Nexus header 2 {:?}", header_2);
-
-        // making 100 submit proof transactions
-        let submit_proof_file = File::open("src/submit_proof_transactions/submit_proof_txn.json").unwrap();
-        let submit_proof_txn_reader = BufReader::new(submit_proof_file);
-        let submit_proof_txn: Transaction = from_reader(submit_proof_txn_reader).unwrap();
-
-        let random_proof = match submit_proof_txn.params {
-            TxParams::SubmitProof(proof) => proof.proof,
-            _ => panic!("Invalid transaction"),
-        };
-
-        for txn_index in 0..100 {
-            let public_inputs = NexusRollupPI {
-                nexus_hash: header_2.hash(),
-                state_root: H256::from(header_2.state_root.as_fixed_slice().clone()),
-                //TODO: remove unwrap
-                height: header_2.number,
-                start_nexus_hash: H256::zero(), // for now
-                app_id: AppAccountId::from(AppId(txn_index as u32)),
-                img_id: StatementDigest(ADAPTER_ID),
-                rollup_hash: Some(H256::zero()),
-            };
-
-            let tx = Transaction {
-                signature: TxSignature([0u8; 64]),
-                params: TxParams::SubmitProof(SubmitProof {
-                    app_id: AppAccountId::from(AppId(txn_index as u32)),
-                    nexus_hash: header_2.hash(),
-                    state_root: H256::from(header_2.state_root.as_fixed_slice().clone()),
-                    proof: random_proof.clone(), // need to generate the actual proof but for now using a random proof
-                    height: header_2.number,
-                    data: None,
-                }),
-            };
-
-            let json_string = serde_json::to_string_pretty(&tx).unwrap();
-            let file_name = format!("src/submit_proof_transactions/submit_proof_txn_{}.json", txn_index);
-            fs::write(file_name, json_string).unwrap();
-
-            match nexus_api.send_tx(tx).await {
-                Ok(i) => {
-                    println!(
-                        "Submitted proof to update state root on nexus. Response: {:?} Stateroot: {:?}",
-                        i, &public_inputs.state_root
-                    )
-                }
-                Err(e) => {
-                    println!("Error when iniating account: {:?}", e);
-
-                    continue;
-                }
-            }
-        }
+    proof
 }
 
-#[tokio::main]
-async fn main() {
-    #[cfg(any(feature = "sp1"))]
-    env_logger::Builder::from_env("RUST_LOG")
-        .filter_level(log::LevelFilter::Info)
-        .init();
-
-    let prover_modes = vec![ProverMode::NoAggregation]; // for now only NoAggregation Mode
-    let mut init_account_transactions: Vec<Transaction> = vec![];
-    let mut submit_proof_transactions: Vec<Transaction> = vec![];
-
+async fn bench_submit_proof_transactions(prover_mode: ProverMode, state_machine: &mut StateMachine<ZKVM, Proof>, avail_headers: Vec<AvailHeader>, mut header_store: HeaderStore) -> Proof {
+    let mut submit_proof_transactions: Vec<Transaction> = Vec::new();
     for txn_index in 0..100 {
-        let file_name = format!("src/submit_proof_transactions/submit_proof_txn_{}.json", txn_index);
-        let file_content = fs::read_to_string(file_name).unwrap();
-        let tx: Transaction = serde_json::from_str(&file_content).unwrap();
-        init_account_transactions.push(tx);
-    }
-
-    for txn_index in 0..100 {
-        let file_name = format!("src/init_account_transactions/init_account_txn_{}.json", txn_index);
+        let file_name = format!("src/submit_proof_transactions/submit_proof_transaction_{}.json", txn_index);
         let file_content = fs::read_to_string(file_name).unwrap();
         let tx: Transaction = serde_json::from_str(&file_content).unwrap();
         submit_proof_transactions.push(tx);
