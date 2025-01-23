4cfb5821afa1dbd5d84d2c81f7697c533f05a181
Prabhat Verma <prabhatverma329@gmail.com>
2025-01-24 02:50:02 +0530

    add risc0-ethereum and update NexusProofManager

--- examples/zksync_adapter/methods/build.rs
+++ examples/zksync_adapter/methods/build.rs
@@ -1,9 +1,26 @@
 #[cfg(any(feature = "sp1"))]
 use sp1_build::build_program;
 
+#[cfg(any(feature = "risc0"))]
+use risc0_build_ethereum::generate_solidity_files;
+
+// Paths where the generated Solidity files will be written.
+#[cfg(any(feature = "risc0"))]
+const SOLIDITY_IMAGE_ID_PATH: &str = "../../../contracts/src/ImageID.sol";
+
+#[cfg(any(feature = "risc0"))]
+const SOLIDITY_ELF_PATH: &str = "../../../contracts/tests/Elf.sol";
+
 fn main() {
     #[cfg(any(feature = "risc0"))]
-    risc0_build::embed_methods();
+    {
+        let guests = risc0_build::embed_methods();
+        let solidity_opts = risc0_build_ethereum::Options::default()
+            .with_image_id_sol_path(SOLIDITY_IMAGE_ID_PATH)
+            .with_elf_sol_path(SOLIDITY_ELF_PATH);
+
+        generate_solidity_files(guests.as_slice(), &solidity_opts).unwrap();
+    }
 
     #[cfg(any(feature = "sp1"))]
     build_program("./sp1-guest")
