0f0188a36b3ecece5e2c70fa6b09f55d3479acd8
Prabhat Verma <prabhatverma329@gmail.com>
2025-01-24 02:54:54 +0530

    fix typo for Elf.sol generation

--- examples/mock_geth_adapter/methods/build.rs
+++ examples/mock_geth_adapter/methods/build.rs
@@ -2,7 +2,7 @@ use risc0_build_ethereum::generate_solidity_files;
 
 // Paths where the generated Solidity files will be written.
 const SOLIDITY_IMAGE_ID_PATH: &str = "../../../contracts/src/ImageID.sol";
-const SOLIDITY_ELF_PATH: &str = "../../../contracts/tests/Elf.sol";
+const SOLIDITY_ELF_PATH: &str = "../../../contracts/test/Elf.sol";
 
 fn main() {
     let guests = risc0_build::embed_methods();
